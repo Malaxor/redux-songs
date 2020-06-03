b6867765d098c1910360e427d3852bf4ceb050bd
PHILIP COSTACHE <nogobaduk@gmail.com>
2020-06-03 17:29:53 -0400

    connect component and mapStateToProps

--- src/components/SongList.js
+++ src/components/SongList.js
@@ -3,9 +3,11 @@ import { connect } from 'react-redux';
 
 class SongList extends Component {
    render() {
+      console.log(this.props)
       return (
          <div>SongList</div>
-      )   
+      )
    }
 }
-export default SongList;
+const mapStateToProps = state => ({ songs: state.songs });
+export default connect(mapStateToProps)(SongList);
